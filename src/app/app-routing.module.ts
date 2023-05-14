fa1bf75ef760ecb3ac8952a93ab8b2b814a5f397
Harshitha Darimadugu <harshithadarimadugu4@gmail.com>
2023-05-14 14:04:37 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { DoctorslistComponent } from './doctorslist/doctorslist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',component:AdminComponent
+  },
+  {
+    path :'doctorslist', component:DoctorslistComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
